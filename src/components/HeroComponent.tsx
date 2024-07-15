822703bd7178405009a2fbabfba108e3ef302ad7
Perpy-del <perpetualmeninwa@gmail.com>
2024-07-15 13:28:32 +0100

    style: :lipstick: add responsiveness of the hero section

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -1,11 +1,11 @@
 const HeroComponent = () => {
   return (
     <div className="px-5 md:px-10 lg:px-20 pb-5">
-      <div className="p-12 bg-slate-300 dark:bg-[#343a40] rounded-lg">
-        <h1 className="text-5xl w-[40%] leading-[64px] font-semibold italic font-serif">
+      <div className="p-5 lg:p-12 bg-slate-300 dark:bg-[#343a40] rounded-lg">
+        <h1 className="text-5xl lg:w-[40%] leading-[64px] font-semibold italic font-serif">
           Title of a longer featured blog post
         </h1>
-        <p className="w-[40%] py-4 leading-7 text-[18px] font-light">
+        <p className="lg:w-[40%] py-4 leading-7 text-[18px] font-light">
           Multiple lines of text that form the lede, informing new readers
           quickly and efficiently about what&apos;s most interesting in this
           post&apos;s contents.

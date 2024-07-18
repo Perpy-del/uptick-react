2ab4531ffcfd3f7c125e5e939617d1d8eacd8710
Perpy-del <perpetualmeninwa@gmail.com>
2024-07-18 23:49:52 +0100

    feat: :sparkles: add the login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,6 @@
 
 import { ThemeProvider } from '../components/theme-provider';
 import { ModeToggle } from '../components/mode-toggle';
-import RegisterFormComponent from '../components/RegisterFormComponent';
 import { useNavigate } from 'react-router-dom';
 import LoginFormComponent from '../components/LoginFormComponent';
 

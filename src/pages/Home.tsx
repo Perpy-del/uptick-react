19178df76777a116d22f0700d0306b28863ac09b
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-07-24 20:19:08 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,21 +2,19 @@ import BlogComponent from '../components/BlogComponent';
 import CategoryComponent from '../components/CategoryComponent';
 import HeroComponent from '../components/HeroComponent';
 import NavBar from '../components/NavBar';
-// import { useNavigate } from 'react-router-dom';
-// import { useEffect } from 'react';
-// import { useSelector } from 'react-redux';
-// const { user } = useSelector((state: any) => state.auth);
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Home = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (user) {
-  //     navigate('/blog')
-  //   } else {
-  //     navigate('/login');
-  //   }
-  // })
+  useEffect(() => {
+    if (user) {
+      navigate('/blog')
+    } else {
+      navigate('/login');
+    }
+  })
 
   return (
     <div className="bg-slate-50 text-black dark:bg-[#212529] dark:text-white">

49ceb0ae37fc3567ee0eceeafca1b4b6e3edb210
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-07-25 08:17:23 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,19 +2,8 @@ import BlogComponent from '../components/BlogComponent';
 import CategoryComponent from '../components/CategoryComponent';
 import HeroComponent from '../components/HeroComponent';
 import NavBar from '../components/NavBar';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
 
 const Home = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (user) {
-      navigate('/blog')
-    } else {
-      navigate('/login');
-    }
-  })
 
   return (
     <div className="bg-slate-50 text-black dark:bg-[#212529] dark:text-white">
